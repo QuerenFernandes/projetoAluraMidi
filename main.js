700188a969f45c2b91bc8ee8cb7d806ab27fa32a
Queren Hapuque <queren.fernandes@yahoo.com>
2022-12-21 07:22:51 -0300

    criando a funcao para tocar em todas as teclas

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
+/*
+SERIA NECESSÁRIO UM BLOCO DE CÓDIGO DESSE PARA CADA TECLA, EXISTE UMA FORMA MELHOR DE FAZER*/
 function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
+/*
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
+*/
 
-/*function tocaSomClap () {
-    document.querySetector('#som_tecla_clap').play();
-}*/
+const listaDeTeclas = document.querySelectorAll('.tecla');
+let contador = 0;
 
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = tocaSomPom;
+
+    contador = contador + 1;
+
+    console.log(contador);
+}
