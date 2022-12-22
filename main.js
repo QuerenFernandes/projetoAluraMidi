b8ac097744cb6aba4dc007effd6d622d5a8de259
Queren Hapuque <queren.fernandes@yahoo.com>
2022-12-22 07:21:43 -0300

    finalizando while

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /*
 SERIA NECESSÁRIO UM BLOCO DE CÓDIGO DESSE PARA CADA TECLA, EXISTE UMA FORMA MELHOR DE FAZER*/
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 /*
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
@@ -11,9 +11,19 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0;
 
 while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = tocaSomPom;
+
+    const tecla = listaDeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
+    //template string
+    const idAudio =`#som_${instrumento}`;
+    //console.log(idAudio);
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
 
     contador = contador + 1;
 
-    console.log(contador);
+   //console.log(contador);
 }
