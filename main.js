ab2e2df39efdfb04d63e4058deda75d7fb67e105
Queren Hapuque <queren.fernandes@yahoo.com>
2022-12-24 06:52:03 -0300

    adicionando if

--- main.js
+++ main.js
@@ -16,11 +16,15 @@ for (let contador = 0; contador < listaDeTeclas.length;contador++) {
     tecla.onclick = function () {
         tocaSom(idAudio);
     };
+    
+    tecla.onkeydown = function (evento) {
+        console.log(evento.code)
 
-    tecla.onkeydown = function () {
-        tecla.classList.add('ativa');
+        if (evento.code = 'Space') {
+            tecla.classList.add('ativa');
+            
+        }   
     }
-
     tecla.onekeyup = function () {
         tecla.classList.remove('ativa');
     }
